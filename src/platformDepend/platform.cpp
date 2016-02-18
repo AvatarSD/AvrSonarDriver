1e98cf2804c3ae4214ca79aa7cdd82c44aea74f6
AvatarSD <sd1995@ukr.net>
2016-02-18 19:52:04 +0200

    port fix

--- src/platformDepend/platform.cpp
+++ src/platformDepend/platform.cpp
@@ -135,19 +135,45 @@ void init()
 	// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T
 	PORTL=(0<<PORTL7) | (0<<PORTL6) | (0<<PORTL5) | (0<<PORTL4) | (0<<PORTL3) | (0<<PORTL2) | (0<<PORTL1) | (0<<PORTL0);
 
-
 	// Timer/Counter 0 initialization
 	// Clock source: System Clock
 	// Clock value: Timer 0 Stopped
 	// Mode: Normal top=0xFF
 	// OC0A output: Disconnected
 	// OC0B output: Disconnected
-	TCCR0A = (0 << COM0A1) | (0 << COM0A0) | (0 << COM0B1) | (0 << COM0B0)
-			| (0 << WGM01) | (0 << WGM00);
-	TCCR0B = (0 << WGM02) | (0 << CS02) | (0 << CS01) | (0 << CS00);
-	TCNT0 = 0x00;
-	OCR0A = 0x00;
-	OCR0B = 0x00;
+	TCCR0A=(0<<COM0A1) | (0<<COM0A0) | (0<<COM0B1) | (0<<COM0B0) | (0<<WGM01) | (0<<WGM00);
+	TCCR0B=(0<<WGM02) | (0<<CS02) | (0<<CS01) | (0<<CS00);
+	TCNT0=0x00;
+	OCR0A=0x00;
+	OCR0B=0x00;
+
+	// Timer/Counter 1 initialization
+	// Clock source: System Clock
+	// Clock value: 250,000 kHz
+	// Mode: CTC top=ICR1
+	// OC1A output: Disconnected
+	// OC1B output: Disconnected
+	// OC1C output: Disconnected
+	// Noise Canceler: Off
+	// Input Capture on Falling Edge
+	// Timer Period: 60 ms
+	// Timer1 Overflow Interrupt: Off
+	// Input Capture Interrupt: On
+	// Compare A Match Interrupt: On
+	// Compare B Match Interrupt: Off
+	// Compare C Match Interrupt: Off
+//	TCCR1A=(0<<COM1A1) | (0<<COM1A0) | (0<<COM1B1) | (0<<COM1B0) | (0<<COM1C1) | (0<<COM1C0) | (0<<WGM11) | (0<<WGM10);
+//	TCCR1B=(0<<ICNC1) | (0<<ICES1) | (1<<WGM13) | (1<<WGM12) | (0<<CS12) | (1<<CS11) | (1<<CS10);
+//	TCNT1H=0x00;
+//	TCNT1L=0x00;
+//	ICR1H=0x3A;
+//	ICR1L=0x97;
+//	OCR1AH=0x00;
+//	OCR1AL=0x03;
+//	OCR1BH=0x00;
+//	OCR1BL=0x00;
+//	OCR1CH=0x00;
+//	OCR1CL=0x00;
 
 	// Timer/Counter 2 initialization
 	// Clock source: System Clock
@@ -155,19 +181,135 @@ void init()
 	// Mode: Normal top=0xFF
 	// OC2A output: Disconnected
 	// OC2B output: Disconnected
-	ASSR = (0 << EXCLK) | (0 << AS2);
-	TCCR2A = (0 << COM2A1) | (0 << COM2A0) | (0 << COM2B1) | (0 << COM2B0)
-			| (0 << WGM21) | (0 << WGM20);
-	TCCR2B = (0 << WGM22) | (0 << CS22) | (0 << CS21) | (0 << CS20);
-	TCNT2 = 0x00;
-	OCR2A = 0x00;
-	OCR2B = 0x00;
+	ASSR=(0<<EXCLK) | (0<<AS2);
+	TCCR2A=(0<<COM2A1) | (0<<COM2A0) | (0<<COM2B1) | (0<<COM2B0) | (0<<WGM21) | (0<<WGM20);
+	TCCR2B=(0<<WGM22) | (0<<CS22) | (0<<CS21) | (0<<CS20);
+	TCNT2=0x00;
+	OCR2A=0x00;
+	OCR2B=0x00;
+
+	// Timer/Counter 3 initialization
+	// Clock source: System Clock
+	// Clock value: Timer3 Stopped
+	// Mode: Normal top=0xFFFF
+	// OC3A output: Disconnected
+	// OC3B output: Disconnected
+	// OC3C output: Disconnected
+	// Noise Canceler: Off
+	// Input Capture on Falling Edge
+	// Timer3 Overflow Interrupt: Off
+	// Input Capture Interrupt: Off
+	// Compare A Match Interrupt: Off
+	// Compare B Match Interrupt: Off
+	// Compare C Match Interrupt: Off
+	TCCR3A=(0<<COM3A1) | (0<<COM3A0) | (0<<COM3B1) | (0<<COM3B0) | (0<<COM3C1) | (0<<COM3C0) | (0<<WGM31) | (0<<WGM30);
+	TCCR3B=(0<<ICNC3) | (0<<ICES3) | (0<<WGM33) | (0<<WGM32) | (0<<CS32) | (0<<CS31) | (0<<CS30);
+	TCNT3H=0x00;
+	TCNT3L=0x00;
+	ICR3H=0x00;
+	ICR3L=0x00;
+	OCR3AH=0x00;
+	OCR3AL=0x00;
+	OCR3BH=0x00;
+	OCR3BL=0x00;
+	OCR3CH=0x00;
+	OCR3CL=0x00;
+
+	// Timer/Counter 4 initialization
+	// Clock source: System Clock
+	// Clock value: Timer4 Stopped
+	// Mode: Normal top=0xFFFF
+	// OC4A output: Disconnected
+	// OC4B output: Disconnected
+	// OC4C output: Disconnected
+	// Noise Canceler: Off
+	// Input Capture on Falling Edge
+	// Timer4 Overflow Interrupt: Off
+	// Input Capture Interrupt: Off
+	// Compare A Match Interrupt: Off
+	// Compare B Match Interrupt: Off
+	// Compare C Match Interrupt: Off
+	TCCR4A=(0<<COM4A1) | (0<<COM4A0) | (0<<COM4B1) | (0<<COM4B0) | (0<<COM4C1) | (0<<COM4C0) | (0<<WGM41) | (0<<WGM40);
+	TCCR4B=(0<<ICNC4) | (0<<ICES4) | (0<<WGM43) | (0<<WGM42) | (0<<CS42) | (0<<CS41) | (0<<CS40);
+	TCNT4H=0x00;
+	TCNT4L=0x00;
+	ICR4H=0x00;
+	ICR4L=0x00;
+	OCR4AH=0x00;
+	OCR4AL=0x00;
+	OCR4BH=0x00;
+	OCR4BL=0x00;
+	OCR4CH=0x00;
+	OCR4CL=0x00;
+
+	// Timer/Counter 5 initialization
+	// Clock source: System Clock
+	// Clock value: Timer5 Stopped
+	// Mode: Normal top=0xFFFF
+	// OC5A output: Disconnected
+	// OC5B output: Disconnected
+	// OC5C output: Disconnected
+	// Noise Canceler: Off
+	// Input Capture on Falling Edge
+	// Timer5 Overflow Interrupt: Off
+	// Input Capture Interrupt: Off
+	// Compare A Match Interrupt: Off
+	// Compare B Match Interrupt: Off
+	// Compare C Match Interrupt: Off
+	TCCR5A=(0<<COM5A1) | (0<<COM5A0) | (0<<COM5B1) | (0<<COM5B0) | (0<<COM5C1) | (0<<COM5C0) | (0<<WGM51) | (0<<WGM50);
+	TCCR5B=(0<<ICNC5) | (0<<ICES5) | (0<<WGM53) | (0<<WGM52) | (0<<CS52) | (0<<CS51) | (0<<CS50);
+	TCNT5H=0x00;
+	TCNT5L=0x00;
+	ICR5H=0x00;
+	ICR5L=0x00;
+	OCR5AH=0x00;
+	OCR5AL=0x00;
+	OCR5BH=0x00;
+	OCR5BL=0x00;
+	OCR5CH=0x00;
+	OCR5CL=0x00;
 
 	// Timer/Counter 0 Interrupt(s) initialization
-	TIMSK0 = (0 << OCIE0B) | (0 << OCIE0A) | (0 << TOIE0);
+	TIMSK0=(0<<OCIE0B) | (0<<OCIE0A) | (0<<TOIE0);
+
+	// Timer/Counter 1 Interrupt(s) initialization
+	//TIMSK1=(1<<ICIE1) | (0<<OCIE1C) | (0<<OCIE1B) | (1<<OCIE1A) | (0<<TOIE1);
 
 	// Timer/Counter 2 Interrupt(s) initialization
-	TIMSK2 = (0 << OCIE2B) | (0 << OCIE2A) | (0 << TOIE2);
+	TIMSK2=(0<<OCIE2B) | (0<<OCIE2A) | (0<<TOIE2);
+
+	// Timer/Counter 3 Interrupt(s) initialization
+	TIMSK3=(0<<ICIE3) | (0<<OCIE3C) | (0<<OCIE3B) | (0<<OCIE3A) | (0<<TOIE3);
+
+	// Timer/Counter 4 Interrupt(s) initialization
+	TIMSK4=(0<<ICIE4) | (0<<OCIE4C) | (0<<OCIE4B) | (0<<OCIE4A) | (0<<TOIE4);
+
+	// Timer/Counter 5 Interrupt(s) initialization
+	TIMSK5=(0<<ICIE5) | (0<<OCIE5C) | (0<<OCIE5B) | (0<<OCIE5A) | (0<<TOIE5);
+
+	// USART0 initialization
+	// Communication Parameters: 8 Data, 1 Stop, No Parity
+	// USART0 Receiver: On
+	// USART0 Transmitter: On
+	// USART0 Mode: Asynchronous
+	// USART0 Baud Rate: 115200 (Double Speed Mode)
+//	UCSR0A=(0<<RXC0) | (0<<TXC0) | (0<<UDRE0) | (0<<FE0) | (0<<DOR0) | (0<<UPE0) | (1<<U2X0) | (0<<MPCM0);
+//	UCSR0B=(1<<RXCIE0) | (1<<TXCIE0) | (0<<UDRIE0) | (1<<RXEN0) | (1<<TXEN0) | (0<<UCSZ02) | (0<<RXB80) | (0<<TXB80);
+//	UCSR0C=(0<<UMSEL01) | (0<<UMSEL00) | (0<<UPM01) | (0<<UPM00) | (0<<USBS0) | (1<<UCSZ01) | (1<<UCSZ00) | (0<<UCPOL0);
+//	UBRR0H=0x00;
+//	UBRR0L=0x10;
+
+	// USART1 initialization
+	// USART1 disabled
+	UCSR1B=(0<<RXCIE1) | (0<<TXCIE1) | (0<<UDRIE1) | (0<<RXEN1) | (0<<TXEN1) | (0<<UCSZ12) | (0<<RXB81) | (0<<TXB81);
+
+	// USART2 initialization
+	// USART2 disabled
+	UCSR2B=(0<<RXCIE2) | (0<<TXCIE2) | (0<<UDRIE2) | (0<<RXEN2) | (0<<TXEN2) | (0<<UCSZ22) | (0<<RXB82) | (0<<TXB82);
+
+	// USART3 initialization
+	// USART3 disabled
+	UCSR3B=(0<<RXCIE3) | (0<<TXCIE3) | (0<<UDRIE3) | (0<<RXEN3) | (0<<TXEN3) | (0<<UCSZ32) | (0<<RXB83) | (0<<TXB83);
 
 	// Analog Comparator initialization
 	// Analog Comparator: Off
@@ -175,21 +317,32 @@ void init()
 	// connected to the AIN0 pin
 	// The Analog Comparator's negative input is
 	// connected to the AIN1 pin
-	ACSR = (1 << ACD) | (0 << ACBG) | (0 << ACO) | (0 << ACI) | (0 << ACIE)
-			| (0 << ACIC) | (0 << ACIS1) | (0 << ACIS0);
+	ACSR=(1<<ACD) | (0<<ACBG) | (0<<ACO) | (0<<ACI) | (0<<ACIE) | (0<<ACIC) | (0<<ACIS1) | (0<<ACIS0);
 	// Digital input buffer on AIN0: On
 	// Digital input buffer on AIN1: On
-	DIDR1 = (0 << AIN0D) | (0 << AIN1D);
+	DIDR1=(0<<AIN0D) | (0<<AIN1D);
+
+//	// ADC initialization
+//	// ADC Clock frequency: 1000,000 kHz
+//	// ADC Voltage Reference: 2.56V, cap. on AREF
+//	// ADC Auto Trigger Source: Free Running
+//	// Digital input buffers on ADC0: On, ADC1: On, ADC2: On, ADC3: On
+//	// ADC4: On, ADC5: On, ADC6: On, ADC7: On
+//	DIDR0=(0<<ADC7D) | (0<<ADC6D) | (0<<ADC5D) | (0<<ADC4D) | (0<<ADC3D) | (0<<ADC2D) | (0<<ADC1D) | (0<<ADC0D);
+//	// Digital input buffers on ADC8: On, ADC9: On, ADC10: On, ADC11: On
+//	// ADC12: On, ADC13: On, ADC14: On, ADC15: On
+//	DIDR2=(0<<ADC15D) | (0<<ADC14D) | (0<<ADC13D) | (0<<ADC12D) | (0<<ADC11D) | (0<<ADC10D) | (0<<ADC9D) | (0<<ADC8D);
+//	ADMUX=(FIRST_ADC_INPUT & 0x1f) | ADC_VREF_TYPE;
+//	ADCSRA=(1<<ADEN) | (1<<ADSC) | (1<<ADATE) | (0<<ADIF) | (1<<ADIE) | (1<<ADPS2) | (0<<ADPS1) | (0<<ADPS0);
+//	ADCSRB=(((FIRST_ADC_INPUT & 0x20)!=0)<<MUX5) | (0<<ADTS2) | (0<<ADTS1) | (0<<ADTS0);
 
 	// SPI initialization
 	// SPI disabled
-	SPCR = (0 << SPIE) | (0 << SPE) | (0 << DORD) | (0 << MSTR) | (0 << CPOL)
-			| (0 << CPHA) | (0 << SPR1) | (0 << SPR0);
+	SPCR=(0<<SPIE) | (0<<SPE) | (0<<DORD) | (0<<MSTR) | (0<<CPOL) | (0<<CPHA) | (0<<SPR1) | (0<<SPR0);
 
 	// TWI initialization
 	// TWI disabled
-	TWCR = (0 << TWEA) | (0 << TWSTA) | (0 << TWSTO) | (0 << TWEN)
-			| (0 << TWIE);
+	TWCR=(0<<TWEA) | (0<<TWSTA) | (0<<TWSTO) | (0<<TWEN) | (0<<TWIE);
 
 	// Global enable interrupts
 	sei();
@@ -242,11 +395,11 @@ void setupExtInt()
 	// INT5: On
 	// INT5 Mode: Any change
 	// INT6: On
-	// INT6 Mode: Low level
+	// INT6 Mode: Any change
 	// INT7: On
 	// INT7 Mode: Any change
 	EICRA=(0<<ISC31) | (1<<ISC30) | (0<<ISC21) | (1<<ISC20) | (0<<ISC11) | (1<<ISC10) | (0<<ISC01) | (1<<ISC00);
-	EICRB=(0<<ISC71) | (1<<ISC70) | (0<<ISC61) | (0<<ISC60) | (0<<ISC51) | (1<<ISC50) | (0<<ISC41) | (1<<ISC40);
+	EICRB=(0<<ISC71) | (1<<ISC70) | (0<<ISC61) | (1<<ISC60) | (0<<ISC51) | (1<<ISC50) | (0<<ISC41) | (1<<ISC40);
 	EIMSK=(1<<INT7) | (1<<INT6) | (1<<INT5) | (1<<INT4) | (1<<INT3) | (1<<INT2) | (1<<INT1) | (1<<INT0);
 	EIFR=(1<<INTF7) | (1<<INTF6) | (1<<INTF5) | (1<<INTF4) | (1<<INTF3) | (1<<INTF2) | (1<<INTF1) | (1<<INTF0);
 	// PCINT0 interrupt: On
